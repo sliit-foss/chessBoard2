cf3fdcd80a041c0a6c1068d3a9dba09956676708
Chamod Shehanka <hcsperera@outlook.com>
2020-06-02 23:51:59 +0530

    Default NavBar Added

---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface FooterProps {}
+
+const Footer: React.SFC<FooterProps> = () => {
+  return <></>;
+};
+
+export default Footer;
---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/common/Navbar/Navbar";
+
+export interface HomePageProps {}
+
+const HomePage: React.SFC<HomePageProps> = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default HomePage;
