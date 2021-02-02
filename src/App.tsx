ad3bd13ea74b7adf7197dfefe7bade525d292ad7
Hansi_pabasara <hansipabasara1@gmail.com>
2021-02-02 15:04:26 +0530

    Footer Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/Home/HomePage";
+import Footer from "./components/common/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Footer />
     </div>
   );
 }
